b68b6d7b8f1e2ced58e916567aa94137d8dae79f
doky <48897815+iamdoky@users.noreply.github.com>
2022-01-11 00:03:14 +0900

    f

--- src/main/java/com/board_study/service/BoardService.java
+++ src/main/java/com/board_study/service/BoardService.java
@@ -2,10 +2,15 @@ package com.board_study.service;
 
 import com.board_study.dto.board.BoardRequestDto;
 import com.board_study.dto.board.BoardResponseDto;
+import com.board_study.entity.board.Board;
 import com.board_study.entity.board.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +25,36 @@ public class BoardService {
         return boardRepository.save( boardSaveDto.toEntity()).getId();
     }
 
-    @Transactional(readOnly = true)
+    @Transactional( readOnly = true )
+    public HashMap< String, Obejct > findAll ( Integer page, Integer size ) {
+
+        HashMap<String, Object> resultMap = new HashMap<String, Object>();
+        Page<Board> list = boardRepository.findAll(PageRequest.of( page, size ));
+
+        resultMap.put( "list",
+                list.stream()
+                .map(BoardResponseDto::new)
+                .collect(Collectors.toList()));
+        resultMap.put( "paging", list.getPageable() );
+        resultMap.put( "totalCnt", list.getTotalElements() );
+        resultMap.put( "totalPage", list.getTotalPages() );
+
+        return resultMap;
+    }
+
+    public BoardResponseDto findById( Long id ) {
+        return new BoardResponseDto( boardRepository.findById(id).get() );
+    }
+
+    public int updateBoard( BoardRequestDto boardRequestDto ) {
+        return boardRepository.updateBoard( boardRequestDto );
+    }
+
+    public void deleteById ( Long id ) {
+        boardRepository.deleteById( id );
+    }
+
+    @Transactional( readOnly = true )
     public List<BoardResponseDto> findAll() {
         return boardRepository.findAll().stream().map(BoardResponseDto::new).collect(Collectors.toList());
     }
---
+++ src/main/java/com/board_study/web/BoardController.java
@@ -0,0 +1,77 @@
+package com.board_study.web;
+
+import com.board_study.dto.board.BoardRequestDto;
+import com.board_study.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author : HP
+ * @version 1.0
+ * <p>
+ * Copyright (C) 2010 by G-INNO  All right reserved.
+ * @Class Name : BoardController
+ * @Description :
+ * @Modification Information
+ * <p>
+ * 수정일           수정자          수정내용
+ * -------------------------------------------------------
+ * 2022.01.10      HP        수정내용 1
+ * 2010-04-10      O O O        수정내용 2
+ * @since 2022.01.10
+ */
+
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/board/list")
+    public String getBoardListPage(
+            Model model,
+            @RequestParam( required = false, defaultValue = "0" ) Integer page,
+            @RequestParam( required = false, defaultValue = "5" ) Integer size ) throws Exception {
+        try {
+            model.addAttribute("resultMap", boardService.findAll( page, size ) );
+        } catch ( Exception e ) {
+            throw  new Exception( e.getMessage() );
+        }
+
+        return "/board/list";
+    }
+
+    @GetMapping("/board/view")
+    public String getBoardViewPage(Model model, BoardRequestDto boardRequestDto ) throws Exception {
+
+        try {
+            if ( boardRequestDto.getId() != null ) {
+                model.addAttribute("info", boardService.findById(boardRequestDto.getId()));
+            }
+        } catch ( Exception e ) {
+            throw new Exception( e.getMessage() );
+        }
+
+        return "/board/view";
+    }
+
+    @PostMapping("/board/write/action")
+    public String boardWriteAction( Model model, BoardRequestDto boardRequestDto ) throws Exception {
+        try {
+            Long result = boardService.save(boardRequestDto);
+
+            if ( result < 0 ) {
+                throw new Exception( "#Exception boardWriteAction!!" );
+            }
+        } catch ( Exception e ) {
+            throws new Exception( e.getMessage() );
+        }
+
+        return "redirect:/board/list";
+    }
+
+}
