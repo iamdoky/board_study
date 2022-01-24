1ea7edd9edbc3d73363e78bbc7cda4d263ba4b67
iamdoky <kdh6631@gmail.com>
2022-01-24 11:30:07 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/resources/templates/board/list.html

--- src/main/java/com/board_study/entity/board/BoardRepository.java
+++ src/main/java/com/board_study/entity/board/BoardRepository.java
@@ -10,10 +10,28 @@ import org.springframework.transaction.annotation.Transactional;
 public interface BoardRepository extends JpaRepository < Board, Long > {
 
     String UPDATE_BOARD = " UPDATE board_study.board " +
-            "SET TITLE = :#{#boardRequestDto.title}, " +
-            "CONTENT = :#{#boardRequestDto.content}, " +
-            "UPDATE_TIME = NOW() " +
-            "WHERE ID = :#{#boardRequestDto.id}";
+            " SET TITLE = :#{#boardRequestDto.title}, " +
+            " CONTENT = :#{#boardRequestDto.content}, " +
+            " UPDATE_TIME = NOW() " +
+            " WHERE ID = :#{#boardRequestDto.id}";
+
+    static final String UPDATE_BOARD_READ_CNT_INC = " UPDATE board_study.board " +
+            " SET READ_CNT = READ_CNT +1 " +
+            " WHERE ID = :id ";
+
+    static final String DELETE_BOARD = " DELETE FROM board_study.board " +
+            " WHERE ID IN ( :deleteList )";
+
+    @Transactional
+    @Modifying
+    @Query(value = UPDATE_BOARD_READ_CNT_INC, nativeQuery = true )
+    public int updateBoardReadCntInc ( @Param("id") Long id ) ;
+
+    @Transactional
+    @Modifying
+    @Query(value = DELETE_BOARD, nativeQuery = true )
+    public int deleteBoard ( @Param("deleteList") Long[] deleteList ) ;
+
 
     @Transactional
     @Modifying
--- src/main/java/com/board_study/service/BoardService.java
+++ src/main/java/com/board_study/service/BoardService.java
@@ -7,6 +7,7 @@ import com.board_study.entity.board.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,7 +30,8 @@ public class BoardService {
     public HashMap< String, Object > findAll ( Integer page, Integer size ) {
 
         HashMap<String, Object> resultMap = new HashMap<String, Object>();
-        Page<Board> list = boardRepository.findAll(PageRequest.of( page, size ));
+        Page<Board> list = boardRepository.findAll(PageRequest.of( page, size,
+                Sort.by(Sort.Direction.DESC, "registerTime") ));
 
         resultMap.put( "list",
                 list.stream()
@@ -43,6 +45,7 @@ public class BoardService {
     }
 
     public BoardResponseDto findById( Long id ) {
+        boardRepository.updateBoardReadCntInc(id);
         return new BoardResponseDto( boardRepository.findById(id).get() );
     }
 
@@ -54,8 +57,13 @@ public class BoardService {
         boardRepository.deleteById( id );
     }
 
+    public void deleteAll ( Long[] deleteId ) {
+        boardRepository.deleteBoard( deleteId );
+    }
+
     @Transactional( readOnly = true )
     public List<BoardResponseDto> findAll() {
         return boardRepository.findAll().stream().map(BoardResponseDto::new).collect(Collectors.toList());
     }
+
 }
